b11edd89d2b1d3651276c45de448c75ef9b48755
mohanvpatta <mohanvpatta@gmail.com>
2022-10-02 13:35:09 +0530

    frame+collections+frames+transparent

--- src/components/Frames/TheSolarMission2.js
+++ src/components/Frames/TheSolarMission2.js
@@ -35,7 +35,6 @@ const TheSolarMission2 = ({ collection, setCollection }) => {
             <span
               className="keyword"
               onClick={() => {
-                setImg();
                 !found && setFound(true);
               }}
             >
